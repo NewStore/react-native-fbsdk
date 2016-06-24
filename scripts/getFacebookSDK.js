7e5275df123de48c0197a0a28025a45cde86f046
Tim Cheung <tcheung@newstore.com>
2016-06-24 11:55:33 +0200

    download facebook sdk for ios and adjust search path

---
+++ scripts/getFacebookSDK.js
@@ -0,0 +1,21 @@
+const Download = require('download')
+const downloadStatus = require('download-status')
+const pathExists = require('path-exists')
+
+const iOS = {
+  dl: 'https://origincache.facebook.com/developers/resources/?id=facebook-ios-sdk-current.zip',
+  dist: './iOS/Frameworks/FacebookSDK',
+}
+
+pathExists(iOS.dist)
+  .then(exists => {
+    if (exists) {
+      return console.log('FacebookSDK for iOS already downloaded, skipping ...')
+    }
+    console.log(`Downloading Facebook SDK for iOS ...`)
+    return new Download({ extract: true })
+      .get(iOS.dl)
+      .dest(iOS.dist)
+      .use(downloadStatus())
+      .run();
+  })
